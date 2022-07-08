21461193d08f8a122493fc23ded0abc2f9fdf801
peter anyaogu <anyaogupeter601@gmail.com>
2022-07-08 14:42:59 +0100

    failing test pass

--- tests/v-world/index.ts
+++ tests/v-world/index.ts
@@ -1,5 +1,10 @@
 import { expect } from "./chai";
-import { ethers, deployments, getNamedAccounts } from "hardhat";
+import {
+  ethers,
+  deployments,
+  getNamedAccounts,
+  getUnnamedAccounts,
+} from "hardhat";
 // eslint-disable-next-line node/no-missing-import
 import { VWorld } from "../../typechain";
 
@@ -9,24 +14,23 @@ describe("VWorld", function () {
   let owner: any;
 
   this.beforeEach(async function () {
-    accounts = await ethers.getSigners();
-    await deployments.fixture(["VWorld"]);
+    const nonDeployers = await getUnnamedAccounts();
+    accounts = nonDeployers;
     const { deployer } = await getNamedAccounts();
     owner = deployer;
+    await deployments.fixture(["VWorld"]);
     vworldContract = await ethers.getContract("VWorld");
   });
 
   it("Should be able to purchase more than one land", async function () {
     await vworldContract.purchaseLand([1, 2]);
     await vworldContract.purchaseLand([3, 4]);
-    const userLand = await vworldContract.getUserLands(accounts[0].address);
+    const userLand = await vworldContract.getUserLands(owner);
     expect(userLand.length).to.eq(2);
   });
   it("should not purchase a land that does not exist", async function () {
-    async function inexistentPurchase() {
-      await vworldContract.purchaseLand([18, 82]);
-    }
-    expect(inexistentPurchase).to.be.reverted("invalid argument");
+    const tx = await vworldContract.purchaseLand([18, 82]);
+    expect(tx.wait()).to.be.reverted;
   });
   it("Should not allow repurchase of owned land", async function () {
     await vworldContract.purchaseLand([1, 2]);
@@ -42,23 +46,23 @@ describe("VWorld", function () {
   });
   it("Should return the lands owned by a user", async function () {
     await vworldContract.purchaseLand([1, 2]);
-    const userLand = await vworldContract.getUserLands(accounts[0].address);
+    const userLand = await vworldContract.getUserLands(owner);
     expect(userLand.toString()).to.eq("1,2");
   });
   it("Should not allow transfer of land not owned", async function () {
     await vworldContract.purchaseLand([3, 4]);
-    await vworldContract.transferLandOwnership(accounts[1].address, [1, 2]);
-    const userLand = await vworldContract.getUserLands(accounts[1].address);
+    await vworldContract.transferLandOwnership(accounts[1], [1, 2]);
+    const userLand = await vworldContract.getUserLands(accounts[1]);
     expect(userLand.length).to.eq(0);
   });
   it("Should not allow transfer of land that does not exist", async function () {
-    await vworldContract.transferLandOwnership(accounts[3].address, [10, 28]);
-    const userLand = await vworldContract.getUserLands(accounts[3].address);
+    await vworldContract.transferLandOwnership(accounts[3], [10, 28]);
+    const userLand = await vworldContract.getUserLands(accounts[3]);
     expect(userLand.length).to.eq(0);
   });
   it("Should not allow transfer of land not purchased", async function () {
-    await vworldContract.transferLandOwnership(accounts[2].address, [1, 2]);
-    const userLand = await vworldContract.getUserLands(accounts[2].address);
+    await vworldContract.transferLandOwnership(accounts[2], [1, 2]);
+    const userLand = await vworldContract.getUserLands(accounts[2]);
     expect(userLand.length).to.eq(0);
   });
   it("Should return the total no lands", async function () {
